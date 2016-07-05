5ce27689e9350ceb1d4aae4ba88e9ec335614555
Avi <avi.turner111@gmail.com>
2016-07-05 21:41:06 +0300

    Preparation for routes: Added custom map

---
+++ HoloTour/HoloTour/Controls/MapWithRoute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Maps;
+
+namespace HoloTour.Controls
+{
+    
+    public class  MapWithRoute : Map
+    {
+        public List<Position> RouteCoordinates { get;  }
+
+        public MapWithRoute()
+        {
+            RouteCoordinates = new List<Position>();
+        }
+    }
+
+}
