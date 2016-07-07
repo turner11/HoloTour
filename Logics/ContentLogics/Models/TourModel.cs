efb9aba68791a21826b65c80389a341c7c8c8d79
Avi <avi.turner111@gmail.com>
2016-07-07 23:07:29 +0300

    Added images. Runs on android - on windows has an issue of constraints in relative view

--- Logics/ContentLogics/Models/TourModel.cs
+++ Logics/ContentLogics/Models/TourModel.cs
@@ -12,6 +12,16 @@ namespace HoloTour.Models
     public class TourModel
     {
         public string Name { get;  }
+        public byte[] ImageBytes { get; }
+
+        public ImageSource ImageAsImageSource
+        {
+            get
+            {
+                return ImageSource.FromStream(() => new System.IO.MemoryStream(this.ImageBytes));
+
+            }
+        }
         public DateTime Created { get { return DateTime.Today; } }
 
         public Color Color { get { return Color.Purple; } }
@@ -19,16 +29,19 @@ namespace HoloTour.Models
         public ReadOnlyCollection<PointOfInterestModel> PointsOfInterest { get; }
 
         public TourModel(JObject json)
-            :this(json["Name"].Value<string>(), json["PointsOfInterest"].Select(poiJson => new PointOfInterestModel(poiJson as JObject)).ToList())
+            :this(json["Name"].Value<string>(), 
+                 json["ImageBytes"].Value<byte[]>(),
+                  json["PointsOfInterest"].Select(poiJson => new PointOfInterestModel(poiJson as JObject)).ToList())
         {
             //var name = json["Name"].Value<string>();
             //var poisJson = json["PointsOfInterest"];
             //var pois = poisJson.Select(poiJson => new PointOfInterestModel(poiJson)).ToList();
            
         }
-        public TourModel(string name, IEnumerable<PointOfInterestModel> pointsOfInterest)
+        public TourModel(string name,byte[] imageByte, IEnumerable<PointOfInterestModel> pointsOfInterest)
         {
             this.Name = name;
+            this.ImageBytes = imageByte;
             pointsOfInterest = pointsOfInterest ?? new List<PointOfInterestModel>();
             this.PointsOfInterest = new ReadOnlyCollection<PointOfInterestModel>(pointsOfInterest.ToList());
         }
--- UI/HoloTour/Pages/ToursPage.xaml.cs
+++ UI/HoloTour/Pages/ToursPage.xaml.cs
@@ -20,6 +20,9 @@ namespace HoloTour.Pages
 
             InitializeComponent();
             this._lstTours = this.GetBindedList();
+            this._lstTours.BindingContextChanged += lstTours_BindingContextChanged;
+            this._lstTours.SeparatorColor = Color.Blue;
+            this._lstTours.RowHeight = 200;
 
             Label header = new Label
             {
@@ -43,7 +46,7 @@ namespace HoloTour.Pages
             this._lstTours.ItemSelected += lstTours_ItemSelected;
         }
 
-        
+     
 
         private ListView GetBindedList()
         {
@@ -62,50 +65,87 @@ namespace HoloTour.Pages
             // (Argument of DataTemplate constructor is called for 
             //      each item; it must return a Cell derivative.)
 
-            var dummyTour = new TourModel("dummy",null);
-           var tmplate = new DataTemplate(() =>
-            {
+            TourModel dummyTour = null;
+            var tmplate = new DataTemplate(() =>
+             {
                 // Create views with bindings for displaying each property.
                 Label nameLabel = new Label();
-                nameLabel.SetBinding(Label.TextProperty, nameof(dummyTour.Name));
+                 nameLabel.SetBinding(Label.TextProperty, nameof(dummyTour.Name));
+                 nameLabel.HorizontalOptions = LayoutOptions.FillAndExpand;
 
-                Label birthdayLabel = new Label();
-                birthdayLabel.SetBinding(Label.TextProperty,
-                    new Binding(nameof(dummyTour.Created), BindingMode.OneWay,
-                        null, null, "Created At {0:d}"));
 
-                BoxView boxView = new BoxView();
-                boxView.SetBinding(BoxView.ColorProperty, nameof(dummyTour.Color));
+                 Label birthdayLabel = new Label();
+                 birthdayLabel.SetBinding(Label.TextProperty,
+                     new Binding(nameof(dummyTour.Created), BindingMode.OneWay,
+                         null, null, "Created At {0:d}"));
 
-                // Return an assembled ViewCell.
-                return new ViewCell
-                {
-                    View = new StackLayout
-                    {
-                        Padding = new Thickness(0, 5),
-                        Orientation = StackOrientation.Horizontal,
-                        Children =
-                                {
-                                    boxView,
-                                    new StackLayout
-                                    {
-                                        VerticalOptions = LayoutOptions.Center,
-                                        Spacing = 0,
-                                        Children =
-                                        {
-                                            nameLabel,
-                                            birthdayLabel
-                                        }
-                                        }
-                                }
-                    }
-                };
-            });
+                 //BoxView boxView = new BoxView();
+                 //boxView.SetBinding(BoxView.ColorProperty, nameof(dummyTour.Color));
+                 var image = new Image();
+                 image.SetBinding(Image.SourceProperty, nameof(dummyTour.ImageAsImageSource));
+                 image.Aspect = Aspect.Fill;
+                 image.HorizontalOptions = LayoutOptions.Center;
+                 // Return an assembled ViewCell.
+
+                 var boxView = new BoxView();
+                 boxView.Color = Color.White;
+                 //boxView.HeightRequest = 10;
+                 boxView.HorizontalOptions = LayoutOptions.FillAndExpand;
+
+
+                 var outerlayOut = new StackLayout
+                 {
+
+                     BackgroundColor = Color.White,
+                     //Padding = new Thickness(0, 5),
+                     //Orientation = StackOrientation.Horizontal,
+                 };
+
+
+                 var innerLayout = new RelativeLayout();
+             innerLayout.Children.Add(image,
+            xConstraint: Constraint.Constant(0),
+            yConstraint: Constraint.Constant(0),
+            widthConstraint: Constraint.RelativeToParent((parent) => { return parent.Width; }),
+            heightConstraint:Constraint.RelativeToParent((parent) => {return parent.Height*3/4;}));
+
+                 innerLayout.Children.Add(nameLabel,
+                     Constraint.Constant(0),
+                     Constraint.Constant(0),
+                     Constraint.RelativeToParent((parent) => { return parent.Width; }),
+                     Constraint.RelativeToParent((parent) => { return parent.Height; }));
+
+                 innerLayout.Children.Add(boxView,
+                      Constraint.Constant(0),
+                        Constraint.Constant(0),
+                     Constraint.RelativeToParent((parent) => { return parent.Width; }),
+                     Constraint.RelativeToParent((parent) => { return parent.Height < 0 ? parent.Height : parent.Height / 4; }));
+
+                 outerlayOut.Children.Add(innerLayout);
+                 return new ViewCell
+                 {
+                    
+                     View = outerlayOut
+                 };
+             });
 
             return tmplate;
-            
+
 
         }
+
+
+
+
+        private void lstTours_BindingContextChanged(object sender, EventArgs e)
+        {
+            base.OnBindingContextChanged();
+            //var tour= (TourModel)this.BindingContext;
+            //var image = this.GetImageForTour(tour);
+            
+            
+        }
+    
 
         private void lstTours_ItemSelected(object sender, SelectedItemChangedEventArgs e)
         {
@@ -118,6 +158,22 @@ namespace HoloTour.Pages
                 //App.Current.MainPage = new MainPage();
             }
         }
+
+        //Dictionary<string, ImageSource> _imageCache = new Dictionary<string, ImageSource>();
+
+        //ImageSource GetImageForTour(TourModel tm)
+        //{
+        //    ImageSource retVal = null;
+
+        //    if (!this._imageCache.TryGetValue(tm.Name, out retVal))
+        //    {
+        //        retVal  = ImageSource.FromStream(() => new System.IO.MemoryStream(tm.ImageBytes));
+        //        this._imageCache[tm.Name] = retVal;
+        //    }
+
+        //    return retVal;
+
+        //}
     }
 }
 
