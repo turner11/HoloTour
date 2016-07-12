b2bc7e8ecd26fa5e54325e0f63b1f065c540c3f1
Avi <avi.turner111@gmail.com>
2016-07-12 15:15:37 +0300

    Work on tour overview Page
    Note: Commented out:
    image.SetBinding(Image.SourceProperty,
    nameof(dummyPoi.ImageAsImageSource));
    since it throws an exception in android. still need to check why

--- UI/HoloTour/Pages/TourOverviewPage.xaml.cs
+++ UI/HoloTour/Pages/TourOverviewPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using HoloTour.ViewModels;
+﻿using HoloTour.Controls;
+using HoloTour.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,18 +13,134 @@ namespace HoloTour.Pages
 {
     public partial class TourOverviewPage : ContentPage
     {
+        private readonly MapWithRoute MapView;
+        private ListView _lstPoi;
+
         TourViewModel _tourViewModel { get; }
         public TourOverviewPage(TourViewModel viewModel)
         {
             InitializeComponent();
             this._tourViewModel = viewModel;
 
+            this._lstPoi = new ListView();
+            this._lstPoi.ItemsSource = this._tourViewModel.PointsOfInterest;
+            this._lstPoi.ItemTemplate= this.GetListTemplate();
+            this._lstPoi.RowHeight = 60;
+
+            this.MapView = new MapWithRoute();
             //add all points of interest to route
             this.MapView.RouteCoordinates.AddRange(this._tourViewModel.PointsOfInterest.Select(poi => poi.Position));
 
-
-            this.lblLocation.IsVisible = false;
+            this.SetLayout();
+            //LayoutOptions.Start
+            //this.lblLocation.IsVisible = false;
             this.Appearing += TourPage_Appearing;
+
+        }
+
+        private DataTemplate GetListTemplate()
+        {
+            PointOfInterestViewModel dummyPoi = null;
+            var tmplate = new DataTemplate(() =>
+            {
+                // Create views with bindings for displaying each property.
+
+                #region Define the view items
+                var image = new Image()
+                {
+                    Aspect = Aspect.Fill, //Aspect.AspectFill,//
+                    HorizontalOptions = LayoutOptions.Fill,// LayoutOptions.Center,
+                                                           //WidthRequest=10000000
+
+                };
+                //image.SetBinding(Image.SourceProperty, nameof(dummyPoi.ImageAsImageSource));
+
+                var boxView = new BoxView()
+                {
+                    Color = Color.White,
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                };
+
+                Label titleLabel = new Label()
+                {
+                    HorizontalOptions = LayoutOptions.FillAndExpand,
+                    TextColor = Color.Black
+
+
+                };
+                titleLabel.SetBinding(Label.TextProperty, nameof(dummyPoi.Title));
+
+                #endregion
+
+
+                //Creates layouts that will hold views
+                var outerlayOut = new StackLayout
+                {
+                    BackgroundColor = Color.White,
+                    Padding = new Thickness(0, 5),
+                    Orientation = StackOrientation.Horizontal,
+
+                };
+                //The inner layout will actualy hold the view items
+                var innerLayout = new RelativeLayout() { BackgroundColor = Color.White };
+                outerlayOut.Children.Add(innerLayout);
+
+                //Add views to layouts
+                innerLayout.Children.Add(image,
+                       xConstraint: Constraint.Constant(0),
+                       yConstraint: Constraint.Constant(0),
+                       widthConstraint: Constraint.RelativeToParent((parent) => parent.Width / 5),
+                       heightConstraint: Constraint.RelativeToParent((parent) => parent.Height)
+                                        );
+
+                innerLayout.Children.Add(boxView,
+                     xConstraint: Constraint.RelativeToView(image, (parent, view) => view.X + view.Width),
+                     yConstraint: Constraint.RelativeToView(image, (parent, view) => view.Y),
+                     widthConstraint: Constraint.RelativeToView(image, (parent, view) => parent.Width - view.Width),
+                     heightConstraint: Constraint.RelativeToView(image, (parent, view) => view.Height)
+                                      );
+
+                innerLayout.Children.Add(titleLabel,
+                    xConstraint: Constraint.RelativeToView(boxView, (parent, view) => view.X + 2),
+                     yConstraint: Constraint.RelativeToView(boxView, (parent, view) => view.Y + view.Height / 2)
+                     );
+
+
+                return new ViewCell
+                {
+
+                    View = outerlayOut
+                };
+            });
+
+            return tmplate;
+        }
+
+        private void SetLayout()
+        {
+            var relativeLayout = new RelativeLayout()
+            {
+                Padding = 10
+            };
+
+
+
+            relativeLayout.Children.Add(this.MapView,
+                 xConstraint: Constraint.Constant(0),
+                yConstraint: Constraint.Constant(0),
+                widthConstraint: Constraint.RelativeToParent((parent) => parent.Width),
+                heightConstraint: Constraint.RelativeToParent((parent) => parent.Height/ 2)
+
+                );
+
+            relativeLayout.Children.Add(this._lstPoi,
+                 xConstraint: Constraint.Constant(0),
+                yConstraint: Constraint.RelativeToView(this.MapView,(parent, view)=>view.Y+view.Height)
+                
+                );
+
+
+            this.Content = relativeLayout;
 
         }
 
@@ -68,17 +185,18 @@ namespace HoloTour.Pages
             var poi = this._tourViewModel.PointsOfInterest.Where(p => p.Position == pin.Position).FirstOrDefault();
 
             var textToDisplay = poi.Guide.Text;
-            this.lblLocation.Text = pin.Label + ": " + textToDisplay;
-            this.lblLocation.IsVisible = true;
+            await DisplayAlert(textToDisplay,"Location data","Close");
+            //this.lblLocation.Text = pin.Label + ": " + textToDisplay;
+            //this.lblLocation.IsVisible = true;
 
-            await Task.Delay(3000).ContinueWith((t) =>
-            {
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    this.lblLocation.Text = "";
-                    this.lblLocation.IsVisible = true;
-                });
-            });
+            //await Task.Delay(3000).ContinueWith((t) =>
+            //{
+            //    Device.BeginInvokeOnMainThread(() =>
+            //    {
+            //        this.lblLocation.Text = "";
+            //        this.lblLocation.IsVisible = true;
+            //    });
+            //});
         }
     }
 }
