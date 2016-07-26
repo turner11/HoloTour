14fb24f939b0b720657e7e8c9ab6a8e9bc806f1f
Avi <avi.turner111@gmail.com>
2016-07-26 19:34:47 +0300

    Started building site for content management

---
+++ ContentManager.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ContentManager.Web.MvcApplication" Language="C#" %>
---
+++ ContentManager.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ContentManager.Web.Startup))]
+namespace ContentManager.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
