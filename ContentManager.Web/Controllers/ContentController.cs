aeda94ea924670c37eac0445ae8bc0ebe6489339
Avi <avi.turner111@gmail.com>
2016-07-28 19:06:04 +0300

    Added base content controller class

---
+++ ContentManager.Web/Controllers/ContentController.cs
@@ -0,0 +1,25 @@
+﻿using HoloTour.ContentLogics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContentManager.Web.Controllers
+{
+    
+    public abstract class ContentController: Controller
+    {
+        readonly static ContentService _contentService;
+        protected ContentService ContentService { get { return _contentService; } }
+        static ContentController()
+        {
+            _contentService = ContentService.Factory();
+        }
+
+        public ContentController()
+        {
+
+        }
+    }
+}
--- ContentManager.Web/Controllers/ContentManagementController.cs
+++ ContentManager.Web/Controllers/ContentManagementController.cs
@@ -8,14 +8,13 @@ using System.Web.Mvc;
 
 namespace ContentManager.Web.Controllers
 {
-    public class ContentManagementController : Controller
+    public class ContentManagementController : ContentController
     {
-        readonly ContentService _contentService;
+        
         readonly TourViewModel[] _tours;
         public ContentManagementController()
         {
-            this._contentService = ContentService.Factory();
-            this._tours = this._contentService.GetTours().Select(t => new TourViewModel(t)).ToArray();
+            this._tours = this.ContentService.GetTours().Select(t => new TourViewModel(t)).ToArray();
         }
         // GET: ContentManagement
         public ActionResult Index()
