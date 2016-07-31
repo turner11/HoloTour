3c02c23d983150df861e8d31f1cc4355bbd008dc
Avi <avi.turner111@gmail.com>
2016-07-31 22:40:39 +0300

    Work in progress
    added partial view for tour item.
    Trying to add a neat gallery for tours

--- ContentManager.Web/App_Start/BundleConfig.cs
+++ ContentManager.Web/App_Start/BundleConfig.cs
@@ -26,6 +26,12 @@ namespace ContentManager.Web
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/slideme2Js").Include(
+                     "~/Scripts/slideme2/jquery.slideme2.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/slideme2Css").Include(
+                   "~/Scripts/slideme2/slideme.css"));
         }
     }
 }
